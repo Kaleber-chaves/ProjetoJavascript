b459ff156b7098a47beaa6e7ff4563d390a26d92
Kaleber-chaves <kaleberchaves@gmail.com>
2022-11-12 18:05:33 -0300

    Primeiro commit

---
+++ src/funcao.js
@@ -0,0 +1,12 @@
+function idadeValida (numero) {
+    if (numero<18 || numero>65) {
+        throw new Error('idade fora dos padrões de aceitação')
+    }
+    else { console.log("A idade " + numero + " é valida!"); }
+
+    return numero;
+}
+try { console.log('Idade valida entre 18 e 65 anos.\n') }
+catch (err) { console.log(err.message); }
+
+module.exports = {idadeValida}
